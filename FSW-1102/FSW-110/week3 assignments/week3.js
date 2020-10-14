8082f8a78e1b2b41edafc7811da9163be6f70f88
Tiffanygray2383 <63433432+Tiffanygray2383@users.noreply.github.com>
2020-10-14 02:16:45 -0400

    week3lateassignment

--- FSW-1102/FSW-110/week3 assignments/week3.js
+++ FSW-1102/FSW-110/week3 assignments/week3.js
@@ -1,14 +1,28 @@
-const africa = document.getElementById("africa")
-console.log(africa)
-africa.innerHTML = "Welcome to my JS site"
+const seafood = document.getElementById("seafood")
+console.log(seafood)
+seafood.innerHTML = "Welcome to JJ's Seafood connection"
 
-const lemonade = document.getElementById('lemonade')
-console.log(lemonade)
-lemonade.innerHTML = "All of this was created with Javascript"
+const shrimp = document.getElementById('shrimp')
+console.log(shrimp)
+shrimp.innerHTML = "Shrimp Taco Tuesday"
 
 var newList = document.createElement("li");                
 var myList = document.createTextNode("myList");        
 myList.appendChild(newList);                             
 document.getElementById("myList").appendChild(newList);
+
+function pagebar()
+{
+  var links=document.getElementById('navbar').getElementsByTagName("a");
+  var current = location.href;
+  for (var i=0; i < links.length; i++)
+  {
+   if(links[i].href == current)
+   {
+      links[i].href = "JJ's Seafood connection";
+      links[i].className='grayStyle';
+   }
+ }
+} 
 
 
